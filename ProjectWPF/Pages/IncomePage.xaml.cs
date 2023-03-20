36c679b2427fee8fae8c78d88dbfd1ca1048418c
Bartosz Janczak <bartek.janczak01@gmail.com>
2023-03-21 00:06:01 +0100

    Finish graphical part for remaining pages, update Home and Reports pages, increase page size.

--- ProjectWPF/Pages/IncomePage.xaml.cs
+++ ProjectWPF/Pages/IncomePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,5 +35,41 @@ namespace ProjectWPF.Pages
             Window window = Window.GetWindow(this);
             window.WindowState = WindowState.Minimized;
         }
+
+        private void SavingsCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            SavingsPercentageComboBox.Visibility = Visibility.Visible;
+            SavingsPercentageLabel.Visibility = Visibility.Visible;
+        }
+        private void SavingsCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SavingsPercentageComboBox.Visibility = Visibility.Hidden;
+            SavingsPercentageLabel.Visibility = Visibility.Hidden;
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (e.Key < Key.D0 || e.Key > Key.D9)
+            {
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                {
+                    if (e.Key != Key.Back && e.Key != Key.Delete && e.Key != Key.Left && e.Key != Key.Right)
+                    {
+                        e.Handled = true;
+                    }
+                }
+            }
+            else
+            {
+                // Sprawdzenie, czy wprowadzona wartość jest liczbą z zakresu 0-100
+                int value;
+                if (!int.TryParse(textBox.Text + e.Key.ToString().Last(), out value) || value < 0 || value > 100)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
     }
 }
